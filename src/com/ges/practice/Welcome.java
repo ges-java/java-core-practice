12861a03648ad878026eef46c8c08a2158ed9f13
je24 <java.erp.24@gmail.com>
2017-01-11 23:36:09 -0500

    Adding base welcome file to master branch

---
+++ src/com/ges/practice/Welcome.java
@@ -0,0 +1,9 @@
+package com.ges.practice;
+
+public class Welcome {
+	
+	public static void main(String[] args) {
+		System.out.println("This is a simple welcome class. Have fun with Java!");
+	}
+
+}
